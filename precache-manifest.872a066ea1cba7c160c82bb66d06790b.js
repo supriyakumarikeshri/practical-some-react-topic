5d3106917f3be3d04a3b55513b596bf653596206
supriyakumarikeshri <supriyakumari2393@gmail.com>
2020-05-18 19:45:02 +0530

    Updates

---
+++ precache-manifest.872a066ea1cba7c160c82bb66d06790b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d17a238c12d6805cae6f18c32aae4de",
+    "url": "/practical-some-react-topic/index.html"
+  },
+  {
+    "revision": "a101fa319cd2a7a8a1a4",
+    "url": "/practical-some-react-topic/static/css/2.deb38e74.chunk.css"
+  },
+  {
+    "revision": "37ca63ef37ff0958ff1d",
+    "url": "/practical-some-react-topic/static/css/main.fb03342b.chunk.css"
+  },
+  {
+    "revision": "a101fa319cd2a7a8a1a4",
+    "url": "/practical-some-react-topic/static/js/2.38278de3.chunk.js"
+  },
+  {
+    "revision": "efcde7b090c282bb5081d4473423d5fe",
+    "url": "/practical-some-react-topic/static/js/2.38278de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ca63ef37ff0958ff1d",
+    "url": "/practical-some-react-topic/static/js/main.d96d60ee.chunk.js"
+  },
+  {
+    "revision": "83eca4c342583d5f5f4a",
+    "url": "/practical-some-react-topic/static/js/runtime-main.59100415.js"
+  }
+]);
